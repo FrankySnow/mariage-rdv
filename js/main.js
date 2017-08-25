d18648613856828f2c28879671061da4a40b48f2
Flo <22062145+FrankySnow@users.noreply.github.com>
2017-08-25 11:54:40 +0200

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-var span     = document.getElementById("secondes")
-var secondes = 5
-var timer    = function(){
-	secondes       -= 1
-	span.innerHTML  = secondes
-	if(secondes === 0) return
-	setTimeout( timer, 1000 )
-}
+var countdown = function(secondes){
+	var span = document.getElementById("secondes")
 
-timer()
+	;(function _countdown(){
+		span.innerHTML = secondes
+		if( secondes === 0 ) return
+		secondes -= 1
+		setTimeout( _countdown, 1000 )
+	})()
+}
